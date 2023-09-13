fcbe3b0e6e4426be283862e9c72e511ad9f1d6fa
jessejp <jessepernila@gmail.com>
2023-09-13 20:20:43 +0300

    feat: initial util function generate grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { generateGameGrid } from "./utils/generateGameGrid";
+
 export default function Home() {
+  const mapWidth = 5;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-      
+      <div className={`grid grid-cols-${mapWidth}`}>
+        {generateGameGrid(mapWidth).map((cell, index) => {
+          return (
+            <div
+              key={index}
+              className="bg-green-600 odd:bg-green-400 w-12 h-12 text-sm"
+            >
+              <span>x:{cell.x}</span>
+              <span>y:{cell.y}</span>
+            </div>
+          );
+        })}
+      </div>
     </main>
-  )
+  );
 }
