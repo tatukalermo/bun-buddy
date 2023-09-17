83b3c5b6a79f024777bc9c8fcb85b01377f101b8
jessejp <jessepernila@gmail.com>
2023-09-17 11:51:13 +0300

    websocket lobby initial Bun serve methods

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import GameMapGrid from "@/components/game/GameMapGrid";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:4000/api/socket/lobby");
+    console.log(socket);
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col justify-center items-center bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300">
       <GameMapGrid mapWidth={5} />
---
+++ pages/api/socket/lobby.ts
@@ -0,0 +1,65 @@
+let users: { username: string }[] = [];
+const messages: { username: string; message: string }[] = [];
+
+interface ClientWebSocketData {
+  username: string;
+}
+
+Bun.serve<ClientWebSocketData>({
+  port: 4000,
+  fetch(req, server) {
+    const success = server.upgrade(req, {
+      // Set username
+      data: {
+        username: "käyttäjä_" + Math.random().toString(16).slice(12),
+      },
+    });
+
+    return success
+      ? undefined
+      : new Response("Upgrade failed :(", { status: 500 });
+  },
+  websocket: {
+    // New Client connects to the websockets
+    open(ws) {
+      const newUsername = { username: ws.data.username };
+      users.push(newUsername);
+
+      // Subscribe to pub/sub channel to send/receive broadcast messages,
+      // without this the socket could not esnd event to other clients
+      ws.subscribe("chat");
+
+      // Broadcast that a user joined
+      // On the client side we can parse various messages by the type property
+      ws.publish(
+        "chat",
+        JSON.stringify({ type: "USERS_ADD", data: newUsername })
+      );
+
+      ws.send(JSON.stringify({ type: "USERS_SET", data: users }));
+      ws.send(JSON.stringify({ type: "MESSAGES_SET", data: messages }));
+    },
+    // Client sends a message
+    message(ws, msg) {
+      // Data sent is a string, parse to object
+      const newMessage = JSON.parse(msg);
+      newMessage.username = ws.data.username;
+      messages.push(newMessage);
+
+      // Send message to all clients subscribed to the chat channel
+      ws.publish(
+        "chat",
+        JSON.stringify({ type: "MESSAGES_ADD", data: newMessage })
+      );
+    },
+    // a client disconnects from the server
+    close(ws) {
+      users = users.filter((user) => user.username !== ws.data.username);
+
+      ws.publish(
+        "chat",
+        JSON.stringify({ type: "USERS_REMOVE", data: ws.data.username })
+      );
+    },
+  },
+});
